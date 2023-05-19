fd43ba889301cd8540d1df757117b09d7322d3aa
BabarKhanDev <b.khan01@hotmail.co.uk>
2023-05-19 20:52:34 +0100

    reformat the card ui

--- web_ui/prototypes/library/generate_page.js
+++ web_ui/prototypes/library/generate_page.js
@@ -1,17 +1,12 @@
-function generate_card(total, imgsrc, holo=false){
-
-    let total_element = document.createElement("p")
-    total_element.setAttribute("class", "card_total")
-    total_element.innerHTML = "You have: " + total
-
+function generate_card(name, total, imgsrc, holo=false){
     let img_element = document.createElement("img")
     img_element.setAttribute("alt", "Image of " + name)
+    img_element.setAttribute("class", "card_image")
     img_element.setAttribute("src", imgsrc)
 
     let card_element = document.createElement("div")
     card_element.setAttribute("class", "card")
     card_element.appendChild(img_element)
-    card_element.appendChild(total_element)    
 
     return card_element
 }
@@ -23,8 +18,6 @@ function update_card_scale(scale){
 
     r.style.setProperty('--width', String(250 * scale) + "px");
     r.style.setProperty('--height', String(350 * scale) + "px");
-    r.style.setProperty('--text_large', String(1.1 * scale) + "em");
-    r.style.setProperty('--text_small',  String(0.8 * scale) + "em");
 }
 
 function main(){
@@ -32,7 +25,7 @@ function main(){
     
     for (let i= 0; i < 11; i++){
 
-        card_element = generate_card(i, "../cards/apple.jpg")
+        card_element = generate_card("apple", i, "../cards/apple.jpg")
         card_library.appendChild(card_element)
 
     }
