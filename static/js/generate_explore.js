9a239025c449de97551292d10271aa5edfa5938b
BabarKhanDev <b.khan01@hotmail.com>
2024-05-08 22:26:06 +0100

    Fix /explore page
    Change parameter names
    Update formatting

--- static/js/generate_explore.js
+++ static/js/generate_explore.js
@@ -1,12 +1,11 @@
-async function main(){
-    
+async function main() {
+
     // Set up environment
-    card_api_url = "http://127.0.0.1:5000"
     body = document.getElementById("sets")
 
     // Get the sets
-    let response_sets = await fetch(card_api_url + "/all_sets")
-    let set_data = await response_sets.json();
+    let response = await fetch("/all_sets")
+    let set_data = await response.json();
 
     // When we first see a series we want to create a section for it
     found_series = new Set()
@@ -15,21 +14,21 @@ async function main(){
     Object.entries(set_data).forEach((entry) => {
         let series = entry[1]["series"]
 
-        if (!(found_series.has(series))){
+        if (!(found_series.has(series))) {
             create_section(series, body)
             found_series.add(series)
         }
-        
-        let set_image_url = entry[1]["image_URL"]
-        let set_name = entry[1]["name"]
-        let set_id = entry[1]["id"]
-        
-        make_set_button(set_image_url, set_name, series, set_id) 
+
+        let image_url = entry[1]["image_url"]
+        let name = entry[1]["name"]
+        let id = entry[1]["id"]
+
+        make_set_button(image_url, name, series, id)
     });
 
 }
 
-function create_section(series, body){
+function create_section(series, body) {
 
     let series_section = document.createElement("section")
     series_section.setAttribute("id", series)
@@ -42,31 +41,31 @@ function create_section(series, body){
     let series_label = document.createElement("h2")
     series_label.innerHTML = series[0].toUpperCase() + series.slice(1).toLowerCase()
     series_label.setAttribute("class", "series_label")
-    
+
     series_section.appendChild(series_label)
     series_section.appendChild(series_sets_container)
     body.appendChild(series_section)
-    
+
 }
 
-function make_set_button(set_image_url, set_name, series, id){
+function make_set_button(image_url, name, series, id) {
 
     let set_container = document.createElement("a")
     set_container.setAttribute("class", "set_container")
-    set_container.setAttribute("href", "../explore/"+id)
+    set_container.setAttribute("href", "../explore/" + id)
 
     let set_element = document.createElement("img")
-    set_element.setAttribute("alt", set_name + "logo")
+    set_element.setAttribute("alt", name + "logo")
     set_element.setAttribute("class", "set_image")
-    set_element.setAttribute("src", set_image_url)
+    set_element.setAttribute("src", image_url)
 
     let set_text = document.createElement("p")
     set_text.setAttribute("class", "set_text")
-    set_text.innerHTML = set_name
+    set_text.innerHTML = name
 
     set_container.appendChild(set_element)
     set_container.appendChild(set_text)
 
-    series_container = document.getElementById(series+"_sets")
+    let series_container = document.getElementById(series + "_sets")
     series_container.appendChild(set_container)
 }
